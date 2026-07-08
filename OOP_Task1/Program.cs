e3b650008b9914113e63f46d7b8a6b77b1e89597
SkyGet0 <239184206+SkyGet0@users.noreply.github.com>
2026-07-08 15:41:24 +0300

    Создал решение для первой задачи ООП. Написал первую версию кода для задачи 1.

---
+++ OOP_Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using OOP_Task1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Прямоугольник с верными данными.
+        Rectangle rectangle = new Rectangle(0, 0, 10, 5);
+
+        // Прямоугольник с неверными данными. Должно выбросить исключение.
+        // Rectangle rectangle2 = new Rectangle(0, 0, 0, 5);
+        // Console.WriteLine(rectangle2.CalculateArea());
+
+        Console.WriteLine($"Создан прямоугольнк: {nameof(rectangle)}.");
+        Console.WriteLine($"Координаты левой верхней вершины: ({rectangle.X}, {rectangle.Y}).");
+        Console.WriteLine($"Высота = {rectangle.Height}. Ширина = {rectangle.Width}.");
+        Console.WriteLine($"Площадь {nameof(rectangle)} равна {rectangle.CalculateArea()}.");
+        Console.WriteLine($"Периметр {nameof(rectangle)} равен {rectangle.CalculatePerimeter()}.");
+    }
+}
---
+++ OOP_Task1/Rectangle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Task1
+{
+    public class Rectangle
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        /// <summary>
+        /// Создает прямоугольник с указанными координатами и размером.
+        /// </summary>
+        /// <param name="x">Координата X левой вершины.</param>
+        /// <param name="y">Координата Y левой вершины.</param>
+        /// <param name="width">Ширина прямоугольника. Должна быть больше нуля.</param>
+        /// <param name="height">Высота прямоугольника. Должна быть больше нуля.</param>
+        public Rectangle(int x, int y, int width, int height)
+        {
+            X = x;
+            Y = y;
+            Width = ValidateSide(width);
+            Height = ValidateSide(height);
+        }
+
+        // Проверка значения стороны прямоугольника.
+        private static int ValidateSide(int side)
+        {
+            if (side <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Сторона должна быть положительным числом.");
+            }
+
+            return side;
+        }
+
+        // Расчет периметра прямоугольника.
+        public int CalculatePerimeter()
+        {
+            return (Width + Height) * 2;
+        }
+
+        // Расчет площади прямоугольника.
+        public int CalculateArea()
+        {
+            return Width * Height;
+        }
+    }
+}
